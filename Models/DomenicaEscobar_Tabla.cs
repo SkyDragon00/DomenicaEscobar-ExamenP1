5916759896071eb4b44e1a6573e63d13984a60b5
Domenica Escobar <domenica.escobar.moyano@udla.edu.ec>
2023-10-25 07:21:01 -0600

    Clase y tabla creada

---
+++ Models/DomenicaEscobar_Tabla.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DomenicaEscobar_ExamenP1.Models
+{
+    public class DomenicaEscobar_Tabla
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? DENombre { get; set; }
+        public bool DEPrecio { get; set; }
+        [Range(0.01, 9999.99)]
+        public decimal DEConTocino { get; set; }
+        public DateTime DEFecha { get; set; }
+    }
+}
